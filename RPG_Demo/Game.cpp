3a9d6d4766ca0b839e19fd8b07d1e72f89d2c0f7
patrenaud <1810057@bart.ca>
2018-04-10 15:06:08 -0400

    New commit

---
+++ RPG_Demo/Game.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Run()
+{
+	srand(time(NULL));
+	do
+	{
+		system("cls");
+		std::cout << "Voici les choix disponibles:\n" <<
+			"   -1 : Quitter le jeu\n" <<
+			" Entrer votre choix: ";
+		int choix = 0;
+		std::cin >> choix;
+
+		switch (choix)
+		{
+		case -1:
+			return;
+		}
+	} while (true);
+}
