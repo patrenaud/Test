4c65cbdf134c0bd763fbc4fecb532f9a1a0e7773
patrenaud <1810057@bart.ca>
2018-04-12 18:16:57 -0400

    ok

--- RPG_Demo/Game.cpp
+++ RPG_Demo/Game.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 
 #include "Game.h"
+#include "SquadState.h"
+#include "FightState.h"
+#include "Squad.h"
 
 Game::Game()
 {
@@ -19,15 +22,33 @@ void Game::Run()
 	{
 		system("cls");
 		std::cout << "Voici les choix disponibles:\n" <<
-			"   -1 : Quitter le jeu\n" <<
+			"    1 : Affecter notre squad\n" <<
+			"    2 : Aller combattre!\n" << 
+			"   -1 : Quitter le jeu\n" << 
 			" Entrer votre choix: ";
 		int choix = 0;
 		std::cin >> choix;
 
+		BaseGameState *state = nullptr;
 		switch (choix)
 		{
 		case -1:
 			return;
+		case 1:
+			state = new SquadState();
+			break;
+		case 2:
+			if (Squad::GetCharacters().size() == 0)
+			{
+				std::cout << "La squad est vide. Veuillez ajouter des characters.\n";
+				break;
+			}
+			
+			state = new FightState();
+			break;
 		}
+
+		delete state;
+	
 	} while (true);
 }
